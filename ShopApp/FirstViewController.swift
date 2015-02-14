e34ef6b46bba8badedcbc06a4a02ce768dfc5d28
jaredMermey <jaredmermey@Jareds-MacBook-Air.local>
2015-02-14 11:43:07 -0500

    Initial Commit

---
+++ ShopApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ShopApp
+//
+//  Created by Jared Mermey on 2/14/15.
+//  Copyright (c) 2015 Jared Mermey. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ShopApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ShopApp
+//
+//  Created by Jared Mermey on 2/14/15.
+//  Copyright (c) 2015 Jared Mermey. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
